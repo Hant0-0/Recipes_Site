4aa4eac8ab3c1683ace4c07b6aea7b3f5a627bf9
Hant0-0 <maxim.manoilo@gmail.com>
2024-02-27 11:24:12 +0200

    Fix doc

--- recipes/recipes/local_settings.py
+++ recipes/recipes/local_settings.py
@@ -10,8 +10,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [STATIC_DIR]
+# Static files (CSS, JavaScript, images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'recipes_project', 'static')]
+
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
